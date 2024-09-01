e06c4db548f873bd21f9eb9e6461be4a85841218
George Sevastiadis <sevastiadisg@gmail.com>
2024-09-01 13:39:08 +0300

    create 3 todo cases at the todoReducer

--- todo-app/src/redux/reducers.js
+++ todo-app/src/redux/reducers.js
@@ -1,7 +1,40 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "./actionTypes";
+
 const initialState = {
   todos: [],
   filter: "ALL",
   searchTerm: "",
 };
 
-const todoReducer = (state = initialState, action) => {};
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        todos: [
+          ...state.todos,
+          { text: action.payload.text, completed: false },
+        ],
+        filter: state.filter,
+        searchTerm: state.searchTerm,
+      };
+    case TOGGLE_TODO:
+      return {
+        todos: state.todos.map((todo, index) =>
+          index === action.payload.id
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+        filter: state.filter,
+        searchTerm: state.searchTerm,
+      };
+    case REMOVE_TODO:
+      return {
+        todos: state.todos.filter((todo, index) => index !== action.payload.id),
+        filter: state.filter,
+        searchTerm: state.searchTerm,
+      };
+
+    default:
+      break;
+  }
+};
